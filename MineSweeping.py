66ec77e2d7a446bebbc9890c381bf8c313c5d2eb
Etrav05 <Etravis090705@gmail.com>
2025-10-29 19:58:35 -0400

    Now clicks randomly on the 9x9 gameboard
    Took 4406 trys to complete its first game

--- MineSweeping.py
+++ MineSweeping.py
@@ -11,6 +11,7 @@ import win32api, win32con
 
 gameBoard = 9
 change = 32
+x, y = 271, 271
 
 def click(x, y):
     win32api.SetCursorPos((x, y))
@@ -18,9 +19,8 @@ def click(x, y):
     time.sleep(0.01)
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, 0, 0)
 
+"""  Row by row
 while not keyboard.is_pressed('q'):  ## Kill button
-    x, y = 271, 271
-
     for i in range(gameBoard + 1):
         y = 271 + (change * i)
 
@@ -28,4 +28,22 @@ while not keyboard.is_pressed('q'):  ## Kill button
             if pyautogui.pixel(x, y)[0] == 189:
                 click(x, y)
             x = 271 + (change * i)
+"""
 
+plays = 0
+
+while not keyboard.is_pressed('q'):  ## Kill button
+    random_x = random.randint(0, 8) * 32
+    random_y = random.randint(0, 8) * 32
+
+    click(x + random_x, y + random_y)
+
+    ## check frown/glasses (win/lose)
+    if pyautogui.pixel(398, 200)[0] == 0:
+        print("==== WINNER ====")
+        break
+
+    if pyautogui.pixel(390, 213)[0] == 0:
+        plays += 1
+        click(390, 213)
+        print(f"Restarting, now on game: {plays}")
