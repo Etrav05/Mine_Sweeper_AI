d7900bf4dc5fbd9a99f4bccfccff467a62b50b02
Etrav05 <Etravis090705@gmail.com>
2025-10-29 20:34:45 -0400

    Refactored the modularity of the solution
    Solving, clicking, and end game functions are now defined first, then used in main

--- MineSweeping.py
+++ MineSweeping.py
@@ -1,4 +1,3 @@
-from pyautogui import *
 import pyautogui
 import time
 import keyboard
@@ -13,37 +12,44 @@ gameBoard = 9
 change = 32
 x, y = 271, 271
 
+loseFaceX, loseFaceY = 392, 210
+winFaceX, winFaceY = 401, 196
+
 def click(x, y):
     win32api.SetCursorPos((x, y))
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, 0, 0)
     time.sleep(0.01)
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, 0, 0)
 
-"""  Row by row
-while not keyboard.is_pressed('q'):  ## Kill button
-    for i in range(gameBoard + 1):
-        y = 271 + (change * i)
-
-        for j in range(gameBoard + 1):
-            if pyautogui.pixel(x, y)[0] == 189:
-                click(x, y)
-            x = 271 + (change * i)
-"""
-
-plays = 0
-
-while not keyboard.is_pressed('q'):  ## Kill button
+def randomClicks():
     random_x = random.randint(0, 8) * 32
     random_y = random.randint(0, 8) * 32
 
     click(x + random_x, y + random_y)
 
-    ## check frown/glasses (win/lose)
-    if pyautogui.pixel(398, 200)[0] == 0:
-        print("==== WINNER ====")
-        break
+def winLose(plays):
+    state = False
 
-    if pyautogui.pixel(390, 213)[0] == 0:
+    ## check frown/glasses (win/lose)
+    if pyautogui.pixel(winFaceX, winFaceY)[0] == 0:
+        print("==== WINNER ====")
+        state = True
+
+    if pyautogui.pixel(loseFaceX, loseFaceY)[0] == 0:
         plays += 1
         click(390, 213)
         print(f"Restarting, now on game: {plays}")
+        state = False
+
+    return state, plays
+
+def main():
+    plays = 0
+    state = False
+
+    while not keyboard.is_pressed('q') and not state:  ## Kill button
+        randomClicks()
+        state, plays = winLose(plays)
+
+if __name__ == "__main__":
+    main()
